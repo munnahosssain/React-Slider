a9c1ad7854df0c48e91f6fccb92d5c1030683bfe
Munna Hossain <mayinuddin.munna@gmail.com>
2021-10-13 23:06:53 +0600

    shape done

---
+++ src/component/Slider/Slider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Slider.css'
+export default function Slider() {
+    return (
+        <div className="work" id="work">
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    {/* <img src="" alt="" /> */}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
